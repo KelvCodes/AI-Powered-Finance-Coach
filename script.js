e3556f0e0db611c5a3f894d0c43cab2d505a5679
Kelvin Yeboah <onlykelvin06@gmail.com>
2025-05-09 15:16:13 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-t blob = new Blob([csv.join('\n')], { type: 'text/csv' });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = 'expenses.csv';
-    a.click();
-    URL.revokeObjectURL(url);
-  });
 
-  renderExpenses();
-});
-
-// PWA Service Worker (add this in a separate `service-worker.js` file)
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/service-worker.js');
-}
